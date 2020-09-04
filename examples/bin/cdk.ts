cef28f044b201717ddd6899950ed6598622529b7
Massimiliano Angelino <angmas@amazon.com>
2020-09-05 00:16:24 +0200

    correct licence header

--- examples/bin/cdk.ts
+++ examples/bin/cdk.ts
@@ -1,5 +1,5 @@
 /* 
- *  Copyright 2020 Massimiliano Angelino
+ *  Copyright 2020 Amazon.com or its affiliates
  *  
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
